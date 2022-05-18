e324829141323c2235d29e740deb9d03ce895fc0
miguelgonrod <miguelgonrod2004@gmail.com>
2022-05-17 23:49:10 -0500

    Acabe la funcion modificar y eliminar, tengo a medias refrescar y contar

--- src/main/java/personaApp/personaInterfaz2.java
+++ src/main/java/personaApp/personaInterfaz2.java
@@ -41,9 +41,7 @@ public class personaInterfaz2 extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jDialog1 = new javax.swing.JDialog();
-        jLabel6 = new javax.swing.JLabel();
-        botones = new javax.swing.ButtonGroup();
+        gru_botones = new javax.swing.ButtonGroup();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
@@ -67,27 +65,8 @@ public class personaInterfaz2 extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         tabla = new javax.swing.JTable();
 
-        jLabel6.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
-        jLabel6.setForeground(new java.awt.Color(0, 153, 0));
-        jLabel6.setText("REGISTRO EXITOSO");
-
-        javax.swing.GroupLayout jDialog1Layout = new javax.swing.GroupLayout(jDialog1.getContentPane());
-        jDialog1.getContentPane().setLayout(jDialog1Layout);
-        jDialog1Layout.setHorizontalGroup(
-            jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jDialog1Layout.createSequentialGroup()
-                .addGap(46, 46, 46)
-                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 285, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(69, Short.MAX_VALUE))
-        );
-        jDialog1Layout.setVerticalGroup(
-            jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jDialog1Layout.createSequentialGroup()
-                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 113, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 187, Short.MAX_VALUE))
-        );
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
 
         jLabel1.setText("Nombre");
 
@@ -97,6 +76,7 @@ public class personaInterfaz2 extends javax.swing.JFrame {
 
         jLabel4.setText("Genero");
 
+        gru_botones.add(gen_f);
         gen_f.setText("F");
         gen_f.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -104,6 +84,7 @@ public class personaInterfaz2 extends javax.swing.JFrame {
             }
         });
 
+        gru_botones.add(gen_m);
         gen_m.setText("M");
         gen_m.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -111,6 +92,7 @@ public class personaInterfaz2 extends javax.swing.JFrame {
             }
         });
 
+        gru_botones.add(gen_o);
         gen_o.setText("O");
         gen_o.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -210,18 +192,17 @@ public class personaInterfaz2 extends javax.swing.JFrame {
                             .addComponent(txt_nombre, javax.swing.GroupLayout.PREFERRED_SIZE, 289, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(txt_apellido, javax.swing.GroupLayout.PREFERRED_SIZE, 289, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                        .addComponent(jButton4)
-                                        .addComponent(jButton1))
-                                    .addComponent(gen_m))
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jButton4)
+                                    .addComponent(jButton1)
+                                    .addComponent(gen_m, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE))
                                 .addGap(51, 51, 51)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(gen_f)
                                     .addGroup(layout.createSequentialGroup()
-                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                            .addComponent(jButton2, javax.swing.GroupLayout.Alignment.TRAILING)
-                                            .addComponent(jButton5, javax.swing.GroupLayout.Alignment.TRAILING))
+                                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addComponent(jButton5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                            .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                                         .addGap(71, 71, 71)
                                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                             .addComponent(jButton3, javax.swing.GroupLayout.Alignment.TRAILING)
@@ -299,28 +280,45 @@ public class personaInterfaz2 extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-        botones.add(gen_m);
-        botones.add(gen_f);
-        botones.add(gen_o);
+        gru_botones.add(gen_m);
+        gru_botones.add(gen_f);
+        gru_botones.add(gen_o);
         try{
-            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/persona_22_db", "miguel", "200423");
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/persona_22_db", "persona_app", "123");
             PreparedStatement pst = cn.prepareStatement("INSERT INTO persona VALUES (?,?,?,?,?)");
             
             pst.setString(1, txt_cc.getText().trim());
             pst.setString(2, txt_nombre.getText().trim());
             pst.setString(3, txt_apellido.getText().trim());
             pst.setString(4, txt_edad.getText().trim());
-            if(gen_m.isSelected()){pst.setString(5, "M");} // revisar porque selecciona varios a la vez
+            if(gen_m.isSelected()){pst.setString(5, "M");}
             else if(gen_f.isSelected()){pst.setString(5, "F");}
             else if(gen_o.isSelected()){pst.setString(5, "O");}
             
-            pst.executeUpdate();
-            
-            txt_cc.setText("");
-            txt_nombre.setText("");
-            txt_apellido.setText("");
-            txt_edad.setText("");
-            JOptionPane.showMessageDialog(null,"El registro se ha guardado correctamente");
+            if(!txt_cc.getText().trim().equals("") && !txt_nombre.getText().trim().equals("") && !txt_apellido.getText().trim().equals("") && (gen_m.isSelected() || gen_f.isSelected() || gen_o.isSelected())){
+                pst.executeUpdate();
+
+                txt_cc.setText("");
+                txt_nombre.setText("");
+                txt_apellido.setText("");
+                txt_edad.setText("");
+                JOptionPane.showMessageDialog(null,"El registro se ha guardado correctamente");
+            }
+            else{
+                if(txt_cc.getText().trim().equals("")){
+                    JOptionPane.showMessageDialog(null,"Debe ingresar un numero de cedula");
+                }
+                if(txt_nombre.getText().trim().equals("")){
+                    JOptionPane.showMessageDialog(null,"Debe ingresar un nombre");
+                }
+                if(txt_apellido.getText().trim().equals("")){
+                    JOptionPane.showMessageDialog(null,"Debe ingresar un apellido");
+                }
+                if (!(gen_m.isSelected() || gen_f.isSelected() || gen_o.isSelected())){
+                    JOptionPane.showMessageDialog(null,"Debe selecionar un genero");
+                }
+            }
+   
         }catch(Exception e){
             
         }
@@ -329,7 +327,7 @@ public class personaInterfaz2 extends javax.swing.JFrame {
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
         try{
-            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/persona_22_db","miguel","200423");
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/persona_22_db","persona_app","123");
             PreparedStatement pst = cn.prepareStatement("SELECT * FROM persona WHERE cedula = ?");
             pst.setString(1, txt_cc.getText().trim());
             
@@ -339,7 +337,16 @@ public class personaInterfaz2 extends javax.swing.JFrame {
                 txt_nombre.setText(rs.getString("nombre"));
                 txt_apellido.setText(rs.getString("apellido"));
                 txt_edad.setText(rs.getString("edad"));
-                // txt_nombre.setText(rs.getString("genero")); revisar como seleccionaar los circulos de genero
+                if(rs.getString("genero").equals("M")){
+                    gen_m.setSelected(true);
+                }
+                else if(rs.getString("genero").equals("F")){
+                    gen_f.setSelected(true);
+                }
+                else if(rs.getString("genero").equals("O")){
+                    gen_o.setSelected(true);
+                }
+                // txt_nombre.setText(rs.getString("genero")); revisar como seleccionar los circulos de genero
             }else{
                 JOptionPane.showMessageDialog(null, "Cedula no encontrada");
             }
@@ -351,31 +358,112 @@ public class personaInterfaz2 extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
+        try{
+            
+            String []Datos = new String[3];
+            
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/persona_22_db", "miguel", "200423");
+            PreparedStatement pst = cn.prepareStatement("SELECT * FROM persona");
+            
+            ResultSet rs = pst.executeQuery();
+            
+            while(rs.next()){
+                Datos[0] = "nombre";
+                Datos[1] = "apellido";
+                Datos[2] = "edad";
+                Datos[3] = "genero";
+                Datos[4] = "cedula";
+                
+                modelo.addRow(Datos);
+            }  
+        }catch(Exception e){
+        
+        }
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
         // TODO add your handling code here:
+        try{
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/persona_22_db", "miguel", "200423");
+            PreparedStatement pst = cn.prepareStatement("SELECT COUNT(*) AS total FROM persona");
+            
+            ResultSet rs = pst.executeQuery();
+            
+            txt_nombre.setText(rs.getString("total"));
+            
+            //JOptionPane.showMessageDialog(null, "Hay " + cantidad);
+            
+            
+            
+        }catch(Exception e){
+            
+        }
     }//GEN-LAST:event_jButton4ActionPerformed
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         // TODO add your handling code here:
+        try{
+            String cedula_val = txt_cc.getText().trim();
+            
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/persona_22_db", "persona_app", "123");
+            PreparedStatement pst = cn.prepareStatement("UPDATE persona SET nombre = ?, apellido = ?, edad = ?, genero = ? WHERE cedula = " + cedula_val);
+            
+            pst.setString(1, txt_nombre.getText().trim());
+            pst.setString(2, txt_apellido.getText().trim());
+            pst.setString(3, txt_edad.getText().trim());
+            if(gen_m.isSelected()){
+                pst.setString(4, "M");
+            }
+            else if(gen_f.isSelected()){
+                pst.setString(4, "F");
+            }
+            else if(gen_o.isSelected()){
+                pst.setString(4, "O");
+            }
+            
+            if(!txt_cc.getText().trim().equals("") && !txt_nombre.getText().trim().equals("") && !txt_apellido.getText().trim().equals("") && (gen_m.isSelected() || gen_f.isSelected() || gen_o.isSelected())){
+                pst.executeUpdate();
+
+                txt_cc.setText("");
+                txt_nombre.setText("");
+                txt_apellido.setText("");
+                txt_edad.setText("");
+                JOptionPane.showMessageDialog(null, "Modificacion exitosa");
+            }
+            else{
+                if(txt_cc.getText().trim().equals("")){
+                    JOptionPane.showMessageDialog(null,"Debe ingresar un numero de cedula");
+                }
+                if(txt_nombre.getText().trim().equals("")){
+                    JOptionPane.showMessageDialog(null,"Debe ingresar un nombre");
+                }
+                if(txt_apellido.getText().trim().equals("")){
+                    JOptionPane.showMessageDialog(null,"Debe ingresar un apellido");
+                }
+                if (!(gen_m.isSelected() || gen_f.isSelected() || gen_o.isSelected())){
+                    JOptionPane.showMessageDialog(null,"Debe selecionar un genero");
+                }
+            }
+        }catch(Exception e){
+
+        }
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
         // TODO add your handling code here:
         try{
-            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/persona_22_db","miguel","200423");
+            Connection cn = DriverManager.getConnection("jdbc:mysql://localhost/persona_22_db", "persona_app", "123");
             PreparedStatement pst = cn.prepareStatement("DELETE FROM persona WHERE cedula = ?");
-            pst.setString(1, txt_cc.getText().trim());
             
-            ResultSet rs = pst.executeQuery();
-                    
-            if(rs.next()){
-                JOptionPane.showMessageDialog(null, "Registro eliminado correctamente");
-                // txt_nombre.setText(rs.getString("genero")); revisar como seleccionaar los circulos de genero
-            }else{
-                JOptionPane.showMessageDialog(null, "Cedula no encontrada");
-            }
+            pst.setString(1, txt_cc.getText().trim());
+            pst.executeUpdate();
+            
+            txt_cc.setText("");
+            txt_nombre.setText("");
+            txt_apellido.setText("");
+            txt_edad.setText("");
+            
+            JOptionPane.showMessageDialog(null, "El registro fue eliminado");
             
         }catch(Exception e){
         
@@ -431,10 +519,10 @@ public class personaInterfaz2 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.ButtonGroup botones;
     private javax.swing.JRadioButton gen_f;
     private javax.swing.JRadioButton gen_m;
     private javax.swing.JRadioButton gen_o;
+    private javax.swing.ButtonGroup gru_botones;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
@@ -443,13 +531,11 @@ public class personaInterfaz2 extends javax.swing.JFrame {
     private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
-    private javax.swing.JDialog jDialog1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tabla;
     private javax.swing.JTextField txt_apellido;
